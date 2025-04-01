a3643e76ae081e7e0b8ab215a811e2be58204f27
adhish <adhishpawar@gmail.com>
2025-04-01 17:03:16 +0530

    SpringSec with Basic Auth

---
+++ spring-sec/src/main/java/com/adhish/spring_sec/HelloController.java
@@ -0,0 +1,22 @@
+package com.adhish.spring_sec;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(HttpServletRequest request) {
+        return "Hello World" + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Adhish's Zone" + request.getSession().getId();
+    }
+
+}
