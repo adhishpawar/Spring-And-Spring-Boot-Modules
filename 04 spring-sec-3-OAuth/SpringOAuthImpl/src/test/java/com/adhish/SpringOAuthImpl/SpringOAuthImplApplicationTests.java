f0ce453f896c37a9916a6a9681392fa4ab52d52d
adhish <adhishpawar@gmail.com>
2025-04-07 19:20:03 +0530

    OAuth2 Login Impl

---
+++ 04 spring-sec-3-OAuth/SpringOAuthImpl/src/test/java/com/adhish/SpringOAuthImpl/SpringOAuthImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhish.SpringOAuthImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
